d335b54a96b5abb48a0c9fa95db99d05606abc67
jabou356 <jason.bouffard@mail.mcgill.ca>
2017-03-17 12:29:56 -0400

    minorchanges

--- GitHub_initial_deposit/BatchGenMOT.m
+++ GitHub_initial_deposit/BatchGenMOT.m
@@ -6,7 +6,7 @@ GenericPath
 %% Nom des sujets
 Alias.sujet = sujets_validesJB(Path.ServerAddressE);
 
-for isujet = 1%length(Alias.sujet) : -1 : length(Alias.sujet)-4
+for isujet = length(Alias.sujet) : -1 : 2
     
     disp(['Traitement de ' cell2mat(Alias.sujet(isujet)) ' (' num2str(length(Alias.sujet) - isujet+1) ' sur ' num2str(length(Alias.sujet)) ')'])
         
