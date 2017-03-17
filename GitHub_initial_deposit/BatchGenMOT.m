776fe5a1f96d07008a20f31c96358feda73523e0
jabou356 <jason.bouffard@mail.mcgill.ca>
2017-03-17 15:55:18 -0400

    Changes on various script faire group processing
    OpenSimMOTgeneratorJB: noticed some trials had NaN at the beginning, so
    when filtered all converted in NaNs. fixed by adding a if Nan loop and
    looking for the next valid data point. Replace NaN by the next valid
    data point.
    
    Other: Just adjusting the isujet to get all data

--- GitHub_initial_deposit/BatchGenMOT.m
+++ GitHub_initial_deposit/BatchGenMOT.m
@@ -6,7 +6,7 @@ GenericPath
 %% Nom des sujets
 Alias.sujet = sujets_validesJB(Path.ServerAddressE);
 
-for isujet = length(Alias.sujet) : -1 : 2
+for isujet = length(Alias.sujet) : -1 : 1
     
     disp(['Traitement de ' cell2mat(Alias.sujet(isujet)) ' (' num2str(length(Alias.sujet) - isujet+1) ' sur ' num2str(length(Alias.sujet)) ')'])
         
--- GitHub_initial_deposit/BatchScaling.m
+++ GitHub_initial_deposit/BatchScaling.m
@@ -16,57 +16,17 @@ clear ; close all; clc
 %% Chargement des fonctions
 
 GenericPath
-%% Interrupteurs
-saveresults = 1;
-writeopensim = 1;
 
 %% Nom des sujets
 Alias.sujet = sujets_validesJB(Path.ServerAddressE);
 
-for isujet = length(Alias.sujet)-22   : -1 : 1
+for isujet = length(Alias.sujet)   : -1 : length(Alias.sujet)-21
     
     disp(['Traitement de ' cell2mat(Alias.sujet(isujet)) ' (' num2str(length(Alias.sujet) - isujet+1) ' sur ' num2str(length(Alias.sujet)) ')'])
 %     %% Chemin des fichiers
 SubjectPath
     % Import reconstruction
-    load([Path.exportPath, Alias.sujet{1,isujet} '.mat'], '-mat');
-    
-    
-  %  Alias.Qnames    = dir([Path.importPath '*.Q*']);
-    
-    %% Ouverture et information du modèle
-    % Ouverture du modèle
-    Alias.model    = S2M_rbdl('new',Path.pathModel);
-    % Noms et nombre de DoF
-    Alias.nameDof  = S2M_rbdl('nameDof', Alias.model);
-    Alias.nDof     = S2M_rbdl('nDof', Alias.model);
-    % Noms et nombre de marqueurs
-    Alias.nameTags = S2M_rbdl('nameTags', Alias.model);
-    Alias.nTags    = S2M_rbdl('nTags', Alias.model);
-    % Nom des segments
-    Alias.nameBody = S2M_rbdl('nameBody', Alias.model);
-    [Alias.segmentMarkers, Alias.segmentDoF] = segment_RBDL(2);
-    %Frequence d'échantillonage
-    Alias.frequency=100.00;
-    
-    %% Obtenir les onset et offset de force
- 
-    %% Sauvegarde de la matrice
-    if saveresults == 1
-        save([Path.exportPath Alias.sujet{1,isujet} '.mat'],'Data','Alias')
-    end
-    %clearvars data Data forceindex logical_cells
-    Data=Data(length(Data)) %Je veux seulement conserver le dernier essai (static)
-%     S2M_rbdl_AnimateModel(Alias.model, Data(1).Qdata.Q2)
-    
-    %Générer un fichier .trc pour Opensim
-    if writeopensim == 1
-    OpenSimTRCgeneratorJB 
-    %OpenSimMOTgeneratorJB
+
     setupAndRunScale
-    %setupAndRunIK
-    %setupAndRunMuscleDirection
-    end
-    % Fermeture du model
-    S2M_rbdl('delete', Alias.model);
+ 
 end
