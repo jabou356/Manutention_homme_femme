93f15c1029026206e3db789c9c41f6e52a268fff
jabou356 <jason.bouffard@mail.mcgill.ca>
2017-03-20 11:53:57 -0400

    MuscleFocus

--- GitHub_initial_deposit/MuscleFocusCompute_EMG.m
+++ GitHub_initial_deposit/MuscleFocusCompute_EMG.m
@@ -16,42 +16,61 @@ import org.opensim.modeling.*
 GenericPath
 
 %% Nom des sujets
-GroupAlias.sujet = sujets_validesJB(Path.ServerAddressE);
-data(1).mfEMGmuscle={'Delt_post','Delt_med','Delt_ant','Infra','Supra','Subscap','Gd_dors_IM','Pec_IM'};
-data(1).mfEMGchan=[3 2 1 10 9 11 13 12];
+Alias.sujet = sujets_validesJB(Path.ServerAddressE);
+
 
 % load([Path.ServerAddressE '\Projet_IRSST_LeverCaisse\Jason\data\GroupData\dataEMG.mat']);
-for isujet=length(GroupAlias.sujet):-1:1
+for isujet=length(Alias.sujet):-1:1
     SubjectPath
     
-    Path.EMG=[Path.ServerAddressE '\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\EMG\' GroupAlias.sujet{isujet} '.mat'];
+    Path.EMG=[Path.ServerAddressE '\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\EMG\' Alias.sujet{isujet} '.mat'];
     load(Path.EMG)
+    
+     data(1).mfEMGmuscle={'Delt_post','Delt_med','Delt_ant','Infra','Supra','Subscap','Gd_dors_IM','Pec_IM'};
+data(1).mfEMGchan=[3 2 1 10 9 11 13 12];
     
     binlength=250*freq.emg/1000;
     
-    if mod(binlenght,2)==0 %if bin length is an even number
+    if mod(binlength,2)==0 %if bin length is an even number
         binlength=binlength+1;
     end
     for itrial=1:length(data)
         
-        startEMG=floor(Data(itrial).start)*freq.emg/freq.camera;
-        stopEMG=floor(Data(itrial).end)*freq.emg/freq.camera;
+        startEMG=floor(data(itrial).start)*freq.emg/freq.camera;
+        stopEMG=floor(data(itrial).end)*freq.emg/freq.camera;
         MVTdurationEMG=stopEMG-startEMG;
         
-        data(itrial).normemg=abs(data(itrial).emg)./repmat(MVC,size(data(itrial).emg,1),1);
-        data(itrial).normRBIemg(1:floor(binlength/2),:)=mean(data(itrial).normemg(1:binlength,:),1);
-        data(itrial).normRBIemg(ceiling(length(data(itrial).normemg)-binlength/2):length(data(itrial).normemg),:)=mean(data(itrial).normemg(end-binlength+1:end,:),1);
+        %EMG preprocessing 1: remove DC 
+        data(itrial).emg=data(itrial).emg-repmat(mean(data(itrial).emg,1),size(data(itrial).emg,1),1);
+ 
         
-        
-        for i=ceiling(binlength/2):floor(length(data(itrial).normemg)-binlength/2)
-            data(itrial).normRBIemg(i,:)=mean(data(itrial).normemg(floor(i-binlength/2):ceiling(i+binlength/2),:),1);
+        %EMG preprocessing 2: Bandpass filter
+        [b, a]=butter(2, [10/(freq.emg/2) 425/(freq.emg/2)]);
+        for imuscle=1:size(data(itrial).emg,2)
+        data(itrial).emg(:,imuscle)=filtfilt(b,a,data(itrial).emg(:,imuscle));
         end
         
-        Data(itrial).normRBIemgInt(:,:)=interp1(1:MVTdurationEMG+1,Data(itrial).normRBIemg(startEMG:stopEMG,:),1:(MVTdurationEMG)/999:MVTdurationEMG+1);
+        %EMG preprocessing 3: Rectify and smooth with a zero lag 250 ms rectify bin
+        %averaging
+        data(itrial).normemg=abs(data(itrial).emg)./repmat(MVC,size(data(itrial).emg,1),1);
+        temp=mean(data(itrial).normemg(1:binlength,:),1);
+        data(itrial).normRBIemg(1:floor(binlength/2),:)=repmat(temp,floor(binlength/2),1);
+        temp=mean(data(itrial).normemg(end-binlength+1:end,:),1);
+        data(itrial).normRBIemg(ceil(length(data(itrial).normemg)-binlength/2)+1:length(data(itrial).normemg),:)=repmat(temp,floor(binlength/2),1);
+              
+        for i=ceil(binlength/2):ceil(length(data(itrial).normemg)-binlength/2)
+            data(itrial).normRBIemg(i,:)=mean(data(itrial).normemg(ceil(i-binlength/2):floor(i+binlength/2),:),1);
+        end
+        
+        %Interpolate so the lifting phase is 1000 data point as kinematics
+        data(itrial).normRBIemgInt(:,:)=interp1(1:MVTdurationEMG+1,data(itrial).normRBIemg(startEMG:stopEMG,:),1:(MVTdurationEMG)/999:MVTdurationEMG+1);
         
         
     end
     
+     Path.rbiEMG=[Path.ServerAddressE '\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\rbiEMG\' Alias.sujet{isujet} '.mat'];
+    save(Path.rbiEMG,'data')
+    clear data
 end
 
 
@@ -78,15 +97,15 @@ end
 % clf
 % plot3([0 0],[0 5], [0 0],'k','linewidth',3)
 % hold on
-% plot3([0 Data(itrial).d(i,1,1)],[0 Data(itrial).d(i,2,1)], [0 Data(itrial).d(i,3,1)],'r')
+% plot3([0 data(itrial).d(i,1,1)],[0 data(itrial).d(i,2,1)], [0 data(itrial).d(i,3,1)],'r')
 % plot3([0 vecDir(i,1,1)],[0 vecDir(i,2,1)], [0 vecDir(i,3,1)],'k:','linewidth',2)
-% plot3([0 Data(itrial).d(i,1,2)],[0 Data(itrial).d(i,2,2)], [0 Data(itrial).d(i,3,2)],'b')
+% plot3([0 data(itrial).d(i,1,2)],[0 data(itrial).d(i,2,2)], [0 data(itrial).d(i,3,2)],'b')
 % plot3([0 vecDir(i,1,2)],[0 vecDir(i,2,2)], [0 vecDir(i,3,2)],'k:','linewidth',2)
-% plot3([0 Data(itrial).d(i,1,3)],[0 Data(itrial).d(i,2,3)], [0 Data(itrial).d(i,3,3)],'g')
+% plot3([0 data(itrial).d(i,1,3)],[0 data(itrial).d(i,2,3)], [0 data(itrial).d(i,3,3)],'g')
 % plot3([0 vecDir(i,1,3)],[0 vecDir(i,2,3)], [0 vecDir(i,3,3)],'k:','linewidth',2)
-% plot3([0 Data(itrial).d(i,1,4)],[0 Data(itrial).d(i,2,4)], [0 Data(itrial).d(i,3,4)],'m')
+% plot3([0 data(itrial).d(i,1,4)],[0 data(itrial).d(i,2,4)], [0 data(itrial).d(i,3,4)],'m')
 % plot3([0 vecDir(i,1,4)],[0 vecDir(i,2,4)], [0 vecDir(i,3,4)],'k:','linewidth',2)
-% plot3([0 Data(itrial).d(i,1,5)],[0 Data(itrial).d(i,2,5)], [0 Data(itrial).d(i,3,5)],'c')
+% plot3([0 data(itrial).d(i,1,5)],[0 data(itrial).d(i,2,5)], [0 data(itrial).d(i,3,5)],'c')
 % plot3([0 vecDir(i,1,5)],[0 vecDir(i,2,5)], [0 vecDir(i,3,5)],'k:','linewidth',2)
 %
 % view([0,5,0])
